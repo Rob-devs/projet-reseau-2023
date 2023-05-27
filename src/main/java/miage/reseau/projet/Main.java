edc5bddd96a8aa0268151622af39bdac744203fe
RodolpheDrouet <70568019+RodolpheDrouet@users.noreply.github.com>
2023-05-27 17:30:42 +0200

    Update Main.java
    Adding the commands to the server

--- src/main/java/miage/reseau/projet/Main.java
+++ src/main/java/miage/reseau/projet/Main.java
@@ -15,6 +15,20 @@ public class Main {
     public static void main(String[] args) {
 
         Server server = new Server();
+        server.getCommands().initializeCommands(commands->
+        {
+            commands.addCommand(new Info(server));
+            commands.addCommand(new Set(server));
+            commands.addCommand(new Setnx(server));
+            commands.addCommand(new Expire(server));
+            commands.addCommand(new Exists(server));
+            commands.addCommand(new Append(server));
+            commands.addCommand(new Increment(server));
+            commands.addCommand(new Decrement(server));
+            commands.addCommand(new Get(server));
+            commands.addCommand(new Strlen(server));
+            commands.addCommand(new Delete(server));
+        });
 
         server.start();
     }
